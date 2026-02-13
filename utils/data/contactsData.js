0763c09426da82fa6ff22f9191c94343c4946551
Phoenixdev351 <phoenix.dev351@outlook.com>
2026-02-13 08:48:36 -0800

    remove github link

--- utils/data/contactsData.js
+++ utils/data/contactsData.js
@@ -2,7 +2,7 @@ export const contactsData = {
     email: 'phoenix.dev351@outlook.com',
     // phone: '+8801608797655',
     address: 'Jalan Kiara, Mont Kiara, 50480 Kuala Lumpur, Malaysia',
-    github: 'https://github.com/Phoenixdev351',
+    //github: 'https://github.com/Phoenixdev351',
     // facebook: 'https://www.facebook.com/abusaid.riyaz/',
     // linkedIn: 'https://www.linkedin.com/in/abu-said-bd/',
     // twitter: 'https://twitter.com/said7388',
