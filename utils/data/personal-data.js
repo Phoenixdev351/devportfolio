44dfacc2cdabae4ebab3601df0392973277f3794
Phoenixdev351 <phoenix.dev351@outlook.com>
2026-02-21 01:22:43 -0800

    fix bug

--- utils/data/personal-data.js
+++ utils/data/personal-data.js
@@ -2,7 +2,7 @@ export const personalData = {
   name: "Ruslan Pekar",
   profile: '/profile.png',
   designation: "Software Developer",
-  description: "I would be delighted to meet you via this site. My name is Ruslan Pekar. Professional Software Developer with 5+ years of experience building web applications and backend systems. I have a good idea able to make a profit with you. Have you heard about the 'freelancer' and 'Upwork' site? There are many people who make profits on freelancing on the internet. Upwork, Freelancer offer a variety of services on freelancers. However my country salary is not high, so I am looking for anyone who live in US and can collaborate with me. if you are interested in my opinion, let me know. Please press contact me button and send me a message. I am waiting for you.",
+  description: "I would be delighted to meet you via this site. My name is Ruslan Pekar. Professional Software Developer with 5+ years of experience building web applications and backend systems. I have a good idea able to make a profit with you. Have you heard about the 'freelancer' and 'Upwork' site? There are many people who make profits on freelancing on the internet. Upwork, Freelancer offer a variety of services on freelancers. However my country salary is not high, so I am looking for anyone who can collaborate with me. if you are interested in my opinion, let me know. Please press contact me button and send me a message. I am waiting for you.",
   email: 'phoenix.dev733@outlook.com',
   // phone: '+8801608797655',
   address: 'Yulii Zhemaite St, 1a, Uzhhorod, Zakarpattia Oblast, Ukraine, 88000',
